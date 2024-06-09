1765214b860d0fcaf74206f33cd1f78f22ef0be0
Sayyidah Amalia Rokhimah <106462143+sayyidahbisa@users.noreply.github.com>
2024-06-09 12:18:20 +0800

    Update streamlit_deployment.py

--- streamlit_deployment.py
+++ streamlit_deployment.py
@@ -10,6 +10,7 @@ import streamlit as st
 from PIL import Image
 import numpy as np
 import tensorflow as tf
+import cv2
 
 st.set_page_config(
     page_title="AI Generated Image",
@@ -47,9 +48,13 @@ def main():
     )
 
 
-    st.markdown("<h1 class='rounded-heading'>HueHarmony</h1>", unsafe_allow_html=True)
+    st.markdown(
+        "<h1 class='rounded-heading'>AI Generated Detection</h1>", 
+        unsafe_allow_html=True)
 
-    st.markdown("<h3 style='text-align: center; color: #ff6f61; text-shadow: 0px 2px 5px rgba(0, 0, 0, 0.07);'>🎨 Image Palette Extractor</h3><br>", unsafe_allow_html=True)
+    st.markdown(
+        "<h3 style='text-align: center; color: #ff6f61; text-shadow: 0px 2px 5px rgba(0, 0, 0, 0.07);'>🎨 AI Image Generated Detection</h3><br>", 
+        unsafe_allow_html=True)
 
 
 
@@ -71,11 +76,20 @@ def main():
                 img = Image.open(BytesIO(response.content)).convert("RGB")
             except:
                 st.error("The URL is not valid.")
-
-Generate_pred = st.button("Generate Prediction")
-if Generate_pred:
-  prediction = model.predict(img_reshape).argmax()
-  st.title("Predicted Label for the image is {}".format(map_dict [prediction]))
+    
+    if img_file is not None:
+    # Convert the file to an opencv image.
+      file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
+      opencv_image = cv2.imdecode(file_bytes, 1)
+      opencv_image = cv2.cvtColor(opencv_image, cv2.COLOR_BGR2RGB)
+      resized = cv2.resize(opencv_image,(224,224))
+    # Now do something with the image! For example, let's display it:
+      st.image(opencv_image, channels="RGB")
+    
+    Generate_pred = st.button("Generate Prediction")
+    if Generate_pred:
+      prediction = model.predict(img_file).argmax()
+      st.title("Predicted Label for the image is {}".format(map_dict [prediction]))
 
 if __name__ == '__main__':
     main()
