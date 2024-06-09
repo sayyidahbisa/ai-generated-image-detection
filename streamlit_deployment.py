e589aadaa3bbf6b60af05b40c15dc544c0df332e
Sayyidah Amalia Rokhimah <106462143+sayyidahbisa@users.noreply.github.com>
2024-06-09 17:39:33 +0800

    Update streamlit_deployment.py

--- streamlit_deployment.py
+++ streamlit_deployment.py
@@ -75,10 +75,22 @@ def main():
                 img = Image.open(BytesIO(response.content)).convert("RGB")
             except:
                 st.error("The URL is not valid.")
+                
+    if uploaded_file is not None:
+        # Convert the file to an opencv image.
+        file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
+        opencv_image = cv2.imdecode(file_bytes, 1)
+        opencv_image = cv2.cvtColor(opencv_image, cv2.COLOR_BGR2RGB)
+        resized = cv2.resize(opencv_image,(224,224))
+        # Now do something with the image! For example, let's display it:
+        st.image(opencv_image, channels="RGB")
+
+    resized =img_file(resized)
+    img_reshape = resized[np.newaxis,...]
     
     Generate_pred = st.button("Generate Prediction")
     if Generate_pred:
-      prediction = model.predict(img_file).argmax()
+      prediction = model.predict(img_reshape).argmax()
       st.title("Predicted Label for the image is {}".format(map_dict [prediction]))
         
 if __name__ == '__main__':
